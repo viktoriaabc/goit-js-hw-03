a46b01bfd145ba3bf644b58083dc6939316e9829
Ordinateur 1 <abcvika3@gmail.com>
2026-01-20 22:40:32 +0100

    add fichiers

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+function filterArray(numbers, value) {
+  let numMax = [];
+  for (let number of numbers) {
+  
+    if (number > value) {
+      numMax.push(number);
+      
+    }
+
+  }
+  return numMax;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
